cfa701597127726ccb159de4212238b5fd06f98e
wolfgang <markus_klock@hotmail.com>
2014-10-22 07:29:26 +0200

    Unfucked the code a little bit

--- latencymatrix.py
+++ latencymatrix.py
@@ -26,7 +26,7 @@ def main(argv):
     if opt == '-h':
       print 'latencymatrix.py -f <inputfile>'
       sys.exit()
-    elif opt in ("-f", "--file"):
+    if opt in ("-f", "--file"):
       inputfile = arg
       #read list of hostnames from inputfile to use in the matrix
       try:
@@ -36,7 +36,7 @@ def main(argv):
       except IOError:
         print "There was an error reading", inputfile
         sys.exit()
-    elif opt in ("-i", "--interactive"):
+    if opt in ("-i", "--interactive"):
       interactive = 1
       print "Use interactive login"
   #Start DEBUG
@@ -45,7 +45,18 @@ def main(argv):
   for item in hostmatrix:
     print item
   #Stop DEBUG
+  if interactive:
+      #hostname = raw_input('hostname: ')
+      print "Enter login credentials for hosts"
+      username = raw_input('username: ')
+      password = getpass.getpass('password: ')
+  else:
+    print "derp"
+  for currenthost in hostmatrix:
+    remoteping(currenthost, hostmatrix, username, password)
+  
   return hostmatrix;
+
 def remoteping(currenthost, allhosts, username, password):
   try:
       s = pxssh.pxssh()
@@ -65,35 +76,5 @@ def remoteping(currenthost, allhosts, username, password):
     print str(e)
   return
 
-def setup(hostmatrix):
-  #gather login credentials interactivly
-  interactive = 1
-  if interactive:
-      #hostname = raw_input('hostname: ')
-      print "Enter login credentials for hosts"
-      username = raw_input('username: ')
-      password = getpass.getpass('password: ')
-  else:
-    print "derp"
-  for currenthost in hostmatrix:
-    remoteping(currenthost, hostmatrix, username, password)
-    # try:
-    #   s = pxssh.pxssh()
-    #   s.login (currenthost, username, password)
-    #   for target in hostmatrix:
-    #     if target != currenthost:
-    #       print "mesure RTT between", currenthost, "<--->", target
-    #       s.sendline ("fping -qc 5 " + target)   # do 5 icmp echo
-    #       s.prompt()             # match the prompt
-    #       print s.before          # print everything before the prompt.
-    #   s.logout()
-
-      #print "lol"
-    # try:                                                            
-    #   thread.start_new_thread( remoteping, (currenthost, hostmatrix, username, password) )
-    # except:
-    #   print "Error: unable to start thread"
-  return;
-
 if __name__ == "__main__":
-  setup(main(sys.argv[1:]))
+  main(sys.argv[1:])
